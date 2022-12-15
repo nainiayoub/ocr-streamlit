cb50145a1f73d4d6d83d970c5a42d28ef58f6316
Ayoub NAINIA <nainiaayoub6@gmail.com>
2022-12-15 19:50:02 +0100

    convert from bytes

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ pdf_file = st.file_uploader("Load your PDF file", type="pdf")
 
 
 def images_to_txt(path):
-    images = pdf2image.convert_from_path(path)
+    images = pdf2image.convert_from_bytes(path)
     all_text = []
     for i in images:
         pil_im = i
-        # text = pytesseract.image_to_string(pil_im, lang='eng')
-        ocr_dict = pytesseract.image_to_data(pil_im, lang='eng', output_type=Output.DICT)
+        text = pytesseract.image_to_string(pil_im, lang='eng')
+        # ocr_dict = pytesseract.image_to_data(pil_im, lang='eng', output_type=Output.DICT)
         # ocr_dict now holds all the OCR info including text and location on the image
-        text = " ".join(ocr_dict['text'])
-        text = re.sub('[ ]{2,}', '\n', text)
+        # text = " ".join(ocr_dict['text'])
+        # text = re.sub('[ ]{2,}', '\n', text)
         all_text.append(text)
     return all_text, len(all_text)
 
